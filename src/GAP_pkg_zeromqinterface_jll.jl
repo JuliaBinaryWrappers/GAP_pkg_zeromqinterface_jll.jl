5acd20345e6d6618733ad10d70f56da7918700d0
jlbuild <juliabuildbot@gmail.com>
2026-06-02 18:54:19 +0000

    GAP_pkg_zeromqinterface_jll build 1.180000.0+0

--- src/GAP_pkg_zeromqinterface_jll.jl
+++ src/GAP_pkg_zeromqinterface_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("GAP_pkg_zeromqinterface")
-JLLWrappers.@generate_main_file("GAP_pkg_zeromqinterface", UUID("abf7faeb-cafb-5034-8851-d0d251ebcfa3"))
+JLLWrappers.@generate_main_file("GAP_pkg_zeromqinterface", Base.UUID("abf7faeb-cafb-5034-8851-d0d251ebcfa3"))
 end  # module GAP_pkg_zeromqinterface_jll
